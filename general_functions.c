c3600c3bba6572fd938e29659e30ff8843a83592
VincentBaron <vincentbaron1996@gmail.com>
2020-05-27 13:12:20 +0200

    precision handled

--- general_functions.c
+++ general_functions.c
@@ -6,7 +6,7 @@
 /*   By: vbaron <vbaron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/25 23:44:08 by vbaron            #+#    #+#             */
-/*   Updated: 2020/05/27 12:46:07 by vbaron           ###   ########.fr       */
+/*   Updated: 2020/05/27 13:11:07 by vbaron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ void    ft_printstr(char *str, t_flags *general)
             i++;
         }
     }
-    general->bytes += ft_strlen(str) - i;
+    general->bytes += (general->precision != 0 ? i : ft_strlen(str));
 }
 
 void    ft_printnbr(int n, t_flags *general)
@@ -68,7 +68,7 @@ void    ft_printwidth(t_flags *general, char *s)
 {
     if (general->width < 0)
         general->width *= -1;
-    while (general->width + general->precision - ft_strlen(s) > 0)
+    while (general->width - (general->precision != 0 ? general->precision : ft_strlen(s)) > 0)
     {    
         ft_printchar(' ', general);
         general->width--;
